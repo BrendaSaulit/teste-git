75a58d52856baabe8cfde53f112ea9f9ae0a54d2
BrendaSaulit <brenda.saulit@gmail.com>
2025-02-12 10:31:59 -0300

    Corrigindo chamada do método getTodosOsPosts()

--- src/config/controllers/postsController.js
+++ src/config/controllers/postsController.js
@@ -2,6 +2,6 @@ import getTodosOsPosts from "../models/postsModel.js";
 
 
 export async function listarPosts(req, res) { //export antes da função é uma forma de exportar. É necessario por o async no começo da funçao da funcao para poder usar o await depois (sao um parzinho)
-    const posts = await getTodosOsPosts//criando a variavel posts
+    const posts = await getTodosOsPosts()//criando a variavel posts
     res.status(200).json(posts); //Envia uma resposta HTTP com status 200 (OK   ) passando a variavel posts que recebe os dados para json
 }
